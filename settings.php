71206ab4e4e41317eae723c14b5a7d79369e6c20
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-22 15:56:16 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+	$adresa[0]="1100 Avenue S., #201 Naples, FL 34102"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Naples";
+	$telephone[0]=2396431117 ;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="3333 Renaissance Blvd #220 Bonita Springs, FL 34134 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Bonita Springs";
+	$telephone[1]=2394982040 ;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="6151 Lake Osprey Dr.Sarasota, FL 34240 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Sarasota";
+	$telephone[2]=9419143993 ;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
